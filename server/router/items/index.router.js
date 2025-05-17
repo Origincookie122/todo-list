291d8dbbae47270ebcb9d47fe4c2b6f41c0d4cc6
Origincookie122 <83477409+Origincookie122@users.noreply.github.com>
2025-05-17 12:08:24 -0400

    Refactored ITEMS Router again...

--- server/router/items/index.router.js
+++ server/router/items/index.router.js
@@ -1,22 +1,24 @@
 module.exports = async (req, res, next) => {
     const pool = req.app.database;
     try {
-        let query = 'SELECT * FROM todo_app_items';
+        let query = "SELECT * FROM `todo_app_items`";
         const params = [];
 
-        if (req.query.id && req.query.list_id) {
-            query += ' WHERE item_id = ? AND list_id = ?';
-            params.push(req.query.id, req.query.list_id);
-        } else if (req.query.id) {
-            query += ' WHERE item_id = ?';
+        const conditions = [];
+        if (req.query.id) {
+            conditions.push("`item_id` = ?");
             params.push(req.query.id);
-        } else if (req.query.list_id) {
-            query += ' WHERE list_id = ?';
+        }
+        if (req.query.list_id) {
+            conditions.push("`list_id` = ?");
             params.push(req.query.list_id);
+        }
+        if (conditions.length > 0) {
+            query += " WHERE " + conditions.join(" AND ");
         }
 
         const [rows] = await pool.query(query, params);
-        res.json(rows);
+        await res.json(rows);
     } catch (err) {
         next(err);
     }
