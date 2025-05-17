5b51679b882701287997c6b595ac946883944c8b
Origincookie122 <83477409+Origincookie122@users.noreply.github.com>
2025-05-17 11:10:05 -0400

    Added database to app

--- server/index.js
+++ server/index.js
@@ -2,6 +2,9 @@ const config = require("./config/config.json");
 const routerHandler = require('./handler/routerLoader.js');
 const app = require('express')();
 
+const database = require('./database/database.js');
+app.database = database;
+
 routerHandler(app);
 
 app.listen(config.Server.port, () => {
